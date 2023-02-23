7a0b02ea1c12afb02a93ba64cbc5ee931f060d62
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2023-02-22 20:28:29 -0500

    Hice la C del CRUD,, guardo productos  desde postman

--- back/routes/products.js
+++ back/routes/products.js
@@ -1,9 +1,13 @@
 const express=require("express") //[29]
 const router=express.Router(); //[30] 
 
-const {getProducts} = require("../controllers/productsController")   //[31]
+const {getProducts, newProduct} = require("../controllers/productsController")   //[31]
 
 router.route('/productos').get(getProducts)    //[32] 
+
+router.route('/producto/nuevo').post(newProduct)  //[74] 
+                    //[75] 
+
 
 module.exports=router;   //[33]
 
