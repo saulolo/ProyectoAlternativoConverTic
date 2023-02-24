8a139ac2d2d6580299ab2f45f97013f7e93a2a96
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2023-02-24 15:09:34 -0500

    Instalando recursos e inicializando el front

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+//[112] y [115]
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda de Moda ConverTic Shop</h1>
+      <img src="./images/logoBlackOriginal.png" alt='Aqui va el logo'></img>  
+    </div>
+  );
+}
+
+export default App;
